9254912020dc3bc626f1e450d8cb729f7d0ef4ef
nurshat <nurshatkazimov@gmail.com>
2025-03-18 22:02:55 +0500

    lab7

---
+++ task1.py
@@ -0,0 +1,45 @@
+import pygame
+import time
+
+pygame.init()
+
+WIDTH, HEIGHT = 600, 400
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Mickey Clock")
+
+minute_hand = pygame.image.load(r"C:\Users\User\Desktop\lab7\images\right_hand.png")
+second_hand = pygame.image.load(r"C:\Users\User\Desktop\lab7\images\left_hand.png")
+clock_face = pygame.image.load(r"C:\Users\User\Desktop\lab7\images\mickeyclock.jpg")
+clock_face = pygame.transform.scale(clock_face, (WIDTH, HEIGHT))
+CENTER = (WIDTH // 2, HEIGHT // 2)
+
+running = True
+while running:
+    screen.fill((255, 255, 255))
+    screen.blit(clock_face, (0, 0))
+
+    current_time = time.localtime()
+    minutes = current_time.tm_min
+    seconds = current_time.tm_sec
+
+    minute_angle = -minutes * 6
+    second_angle = -seconds * 6
+
+    rotated_minute = pygame.transform.rotate(minute_hand, minute_angle)
+    rotated_second = pygame.transform.rotate(second_hand, second_angle)
+
+    minute_rect = rotated_minute.get_rect(center=CENTER)
+    second_rect = rotated_second.get_rect(center=CENTER)
+
+    screen.blit(rotated_minute, minute_rect.topleft)
+    screen.blit(rotated_second, second_rect.topleft)
+
+    pygame.display.flip()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.time.delay(1000 // 30)
+
+pygame.quit()
---
+++ task2.py
@@ -0,0 +1,75 @@
+import pygame
+import os
+
+pygame.init()
+
+WIDTH, HEIGHT = 300, 200
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Music Player")
+
+button_size = (50, 50)
+
+play_img = pygame.transform.scale(pygame.image.load(r"C:\Users\User\Desktop\lab7\images\play.png"), button_size)
+pause_img = pygame.transform.scale(pygame.image.load(r"C:\Users\User\Desktop\lab7\images\pause.png"), button_size)
+next_img = pygame.transform.scale(pygame.image.load(r"C:\Users\User\Desktop\lab7\images\next.png"), button_size)
+prev_img = pygame.transform.scale(pygame.image.load(r"C:\Users\User\Desktop\lab7\images\prev.png"), button_size)
+
+button_size = (50, 50)
+play_rect = pygame.Rect(100, 75, *button_size)
+pause_rect = pygame.Rect(160, 75, *button_size)
+next_rect = pygame.Rect(220, 75, *button_size)
+prev_rect = pygame.Rect(40, 75, *button_size)
+
+music_folder = "tracks"
+tracks = [os.path.join(music_folder, f) for f in os.listdir(music_folder) if f.endswith(".mp3")]
+current_track = 0
+
+if tracks:
+    pygame.mixer.music.load(tracks[current_track])
+
+def play_music():
+    pygame.mixer.music.play()
+
+def pause_music():
+    if pygame.mixer.music.get_busy():
+        pygame.mixer.music.pause()
+    else:
+        pygame.mixer.music.unpause()
+
+def next_track():
+    global current_track
+    current_track = (current_track + 1) % len(tracks)
+    pygame.mixer.music.load(tracks[current_track])
+    play_music()
+
+def prev_track():
+    global current_track
+    current_track = (current_track - 1) % len(tracks)
+    pygame.mixer.music.load(tracks[current_track])
+    play_music()
+
+running = True
+while running:
+    screen.fill((255, 255, 255))
+    
+    screen.blit(prev_img, prev_rect.topleft)
+    screen.blit(play_img, play_rect.topleft)
+    screen.blit(pause_img, pause_rect.topleft)
+    screen.blit(next_img, next_rect.topleft)
+    
+    pygame.display.flip()
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if play_rect.collidepoint(event.pos):
+                play_music()
+            elif pause_rect.collidepoint(event.pos):
+                pause_music()
+            elif next_rect.collidepoint(event.pos):
+                next_track()
+            elif prev_rect.collidepoint(event.pos):
+                prev_track()
+
+pygame.quit()
